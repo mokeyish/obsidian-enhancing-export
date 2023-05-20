8420f960d54391056b471284a36d0dda7c23a1e5
YISH <mokeyish@hotmail.com>
2023-05-21 00:05:58 +0800

    :construction_worker: Add github Actions

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,15 +1,29 @@
 import { readFileSync, writeFileSync } from 'fs';
+import { exec } from 'child_process';
+import process from 'process';
 
-const { npm_package_version: targetVersion, npm_package_description: description} = process.env;
+
+const pkg = JSON.parse(readFileSync('package.json', 'utf8'));
+
+const version = process.argv.at(2) ?? pkg.version;
+
+if (version != pkg.version) {
+  pkg.version = version;
+  writeFileSync('package.json', `${JSON.stringify(pkg, null, 2)}\n`);
+  exec('git add package.json');
+}
+
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-manifest.description = description;
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
+manifest.version = pkg.version;
+manifest.description = pkg.description;
+writeFileSync('manifest.json', JSON.stringify(manifest, null, 2));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
-versions[targetVersion] = minAppVersion;
+versions[pkg.version] = minAppVersion;
 writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+
+exec('git add manifest.json versions.json');
